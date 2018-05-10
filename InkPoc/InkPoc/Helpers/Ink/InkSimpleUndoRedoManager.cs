cc6ab0b94b8b1513365c90ee85c1bc7e239d7b0a
David Gómez <v-davgom@microsoft.com>
2018-05-10 15:44:39 +0200

    Create simple undo redo manager

---
+++ InkPoc/InkPoc/Helpers/Ink/InkSimpleUndoRedoManager.cs
@@ -0,0 +1,58 @@
+﻿using InkPoc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Input.Inking;
+
+namespace InkPoc.Helpers.Ink
+{
+    public class InkSimpleUndoRedoManager
+    {
+        private InkStrokeContainer _strokeContainer;
+        private Stack<InkStroke> _undoStack = new Stack<InkStroke>();
+
+        public InkSimpleUndoRedoManager(InkPresenter inkPresenter)
+        {
+            _strokeContainer = inkPresenter.StrokeContainer;
+        }
+
+        public bool CanUndo => _strokeContainer.GetStrokes().Any();
+
+        public bool CanRedo =>_undoStack.Any();
+
+        public void ClearStack() => _undoStack.Clear();
+
+        public void Undo()
+        {
+            if (!CanUndo)
+                return;
+
+            InkService.ClearStrokesSelection(_strokeContainer);
+            var stroke = _strokeContainer.GetStrokes().Last();
+            stroke.Selected = true;
+            _strokeContainer.DeleteSelected();
+            _undoStack.Push(stroke);
+        }
+
+        public void Redo()
+        {
+            if (!CanRedo)
+                return;
+
+            var stroke = _undoStack.Pop();
+            var newStroke = CopyStroke(stroke);
+            _strokeContainer.AddStroke(newStroke);
+        }
+
+        private InkStroke CopyStroke(InkStroke stroke)
+        {
+            var strokeBuilder = new InkStrokeBuilder();
+            strokeBuilder.SetDefaultDrawingAttributes(stroke.DrawingAttributes);
+            var newStroke = strokeBuilder.CreateStrokeFromInkPoints(stroke.GetInkPoints(), stroke.PointTransform);
+
+            return newStroke;
+        }        
+    }
+}
